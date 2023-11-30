127b142e828eb38697c51b51cf3d4651633ffc55
MrRodent <finaali@gmail.com>
2023-11-30 17:38:06 +0200

    zoom test

--- bman/multiplayergamestate.js
+++ bman/multiplayergamestate.js
@@ -5,10 +5,12 @@ import { setCameraX } from "./camera.js";
 import { clearEnemies, enemies, spawnEnemies } from "./enemy.js";
 import { setTextures, initHardWallsCanvas } from "./level.js";
 import { level, exit, levelHeader, entrance, gameOverText, setGlobalPause, tutorial, bigBomb, fadeTransition, bigBombOverlay } from "./main.js";
-import { showGameOverMenu, updateLevelDisplay, updateScoreDisplay } from "./page.js";
+import { showGameOverMenu, updateLevelDisplay, updateP1Score, updatePVPTimerDisplay, updateScoreDisplay } from "./page.js";
 import { clearPlayers, players, resetPlayerPositions, spawnPlayers } from "./player.js";
-import { createTiles, exitLocation} from "./tile.js";
+import { createTiles, exitLocation, powerupLocations} from "./tile.js";
 import { levels, levelWidth, levelHeight, levelType, levelPowerup, softwallPercent, powerupCount } from "./gamestate.js";
+import { getRandomWalkablePoint } from "./utils.js";
+import { randomPowerup } from "./powerup.js";
 
 const PVPlevelData = {
     width: 13,
@@ -18,15 +20,53 @@ const PVPlevelData = {
     powerupCount: 5,
     softwallPercent: 0.2,
 };
+
 export class MultiplayerGame extends Game
 {
     constructor() {
         super();
         this.numPlayers = 2;
+        this.timerHandle = null;
+        this.seconds = 0;
+        this.minutes = 0;
+    }
+
+    startTimer() {
+        this.timerHandle = setInterval(() => {
+            if(++this.seconds % 60 == 0) {
+                ++this.minutes;
+                this.seconds = 0;
+            }
+            updatePVPTimerDisplay(`${this.minutes.toString().padStart(2, '0')}:
+                                  ${this.seconds.toString().padStart(2, '0')}`);
+
+            // TODO: 10 sekkaa on vähän liian useasti.. 
+            // TODO: Spawnataan mieluummin jotain muuta kun speediä
+            // Spawnaa random poweruppeja tietyn ajan välein
+            if(this.seconds % 10 == 0) {
+                const tile = getRandomWalkablePoint();
+                tile.powerup = randomPowerup();
+                tile.hasPowerup = true;
+                powerupLocations.push(tile);
+            }
+
+        }, 1000);
+    }
+
+    over() {
+        if(this.timerHandle) {
+            clearInterval(this.timerHandle);
+            this.timerHandle = null;
+
+            this.seconds = 0;
+            this.minutes = 0;
+            updatePVPTimerDisplay(`${this.minutes.toString().padStart(2, '0')}:
+                                  ${this.seconds.toString().padStart(2, '0')}`);
+        }
     }
 
     newGame() {
-        console.log("MultiplayerGame");
+        this.startTimer();
         stopBirdsong(); // TODO: Halutaanko jotain audiota tänne?
         stopCurrentTrack();
         fadeTransition.fadeIn();
@@ -38,8 +78,9 @@ export class MultiplayerGame extends Game
         this.newLevel();
         spawnPlayers(2);
         this.initLevel();
-        updateLevelDisplay(this.level);
-        updateScoreDisplay(this.score);
+        // TODO: Oma score display
+        //updateLevelDisplay(this.level);
+        //updateScoreDisplay(this.score);
     }
 
     initLevel() {
@@ -125,10 +166,6 @@ export class MultiplayerGame extends Game
     
     checkGameState() {
         // TODO
-    }
-
-    over() {
-    // TODO
     }
 
     // Saving & loading
